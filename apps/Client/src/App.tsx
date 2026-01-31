34d08f9d3c974ac88d0de8adf08171b541dac866
System Zero Dev <user@system-zero.dev>
2026-01-31 14:17:27 -0500

    test_runner: feature: remove unused RoleProtectedRoute import

--- apps/Client/src/App.tsx
+++ apps/Client/src/App.tsx
@@ -2,7 +2,6 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import { Box, CircularProgress } from '@mui/material';
 import { useAuth } from './hooks/useAuth';
 import { ProtectedRoute } from './components/auth/ProtectedRoute';
-import { RoleProtectedRoute } from './components/auth/RoleProtectedRoute';
 import { MainLayout } from './components/layout/MainLayout';
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
